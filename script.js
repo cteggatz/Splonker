dfb59fc916324fad7967457d9b40fab3e17b2bc5
cteggatz <97490831+cteggatz@users.noreply.github.com>
2022-12-19 15:31:36 -0600

    added Delta Time, added importing, added full screen canvas

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-import DebugStack from "./Modules/debug.js"
+import {createText, DebugStack} from "./scripts/debug.js"
+
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
@@ -7,23 +8,29 @@ const ctx = canvas.getContext('2d');
 /*------------init------------*/
 const time = {
   lastTime: Date.now(),
-  dt: 0,
+  dt: new Array(),
 };
 let gamestack = new Array();
-gamestack.push(new )
-
+gamestack.push(
+  new DebugStack(ctx,[
+    createText("Delta Time", time.dt)
+  ])
+  )
 
 /*---------gameloop--------*/
 function draw() {
-  //ctx.fillStyle = "red";
-  //ctx.fillRect(0,0,110,110);
+  ctx.clearRect(0,0, ctx.canvas.width, ctx.canvas.height);
+  gamestack.forEach((obj)=>{obj.draw()})
+  ctx.fillRect(100,100,100,100);
 }
 function update() {}
 function gameLoop(callback) {
-  time.dt = time.lastTime - callback;
+  time.dt[0] = callback-time.lastTime;
+  time.lastTime = callback;
 
   update();
   draw();
+  requestAnimationFrame(gameLoop);
 }
 // ---------init-----------
 window.onload = function () {
--- scripts/debug.js
+++ scripts/debug.js
@@ -1,16 +1,21 @@
-class DebugStack {
+export class DebugStack {
   constructor(ctx, stack) {
     this.ctx = ctx;
     this.stack = stack;
   }
   draw() {
+    this.ctx.font = "10px Georgia";
     for (let i = 0; i<this.stack.length; i++) {
-      ctx.fillStyle = 'green';
-      ctx.fillText(`${e.text}: ${e.value}`,0, 10*i);
+      this.ctx.fillStyle = 'red';
+      this.ctx.fillText(`${this.stack[i].text}: ${this.stack[i].value}`,5, 12);
     }
+    this.ctx.fillStyle = "white"
+    //console.log("drawn")
   }
-  createText(text, value) {
-    return { text: text, value: value };
-  }
+
+}
+export function createText(text, value) {
+  return { text: text, value: value };
 }
 export default DebugStack;
+ 
